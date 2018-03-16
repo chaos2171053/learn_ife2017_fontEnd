8abc436bb20beb4ac989789dd2dcad6a284f22ad
chaos2171053 <chaos2171053@gmail.com>
2018-03-16 17:59:36 +0800

    init

---
+++ 糯米学院/vue双向绑定2/index.js
@@ -0,0 +1,59 @@
+/**
+ * 观察者构造函数
+ * @constructor
+ * @param {any} data 
+ */
+function Observer(data) {
+    this.data = data
+    this.walk(data)
+}
+
+let p = Observer.prototype
+
+
+p.walk = function (obj) {
+    let val
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            val = obj[key]
+            if (typeof val === 'object') {
+                new Observer(val)
+            }
+
+            this.convet(key, val)
+        }
+    }
+}
+p.convet = function (key, val) {
+    Object.defineProperty(this.data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+            if(typeof key ==='object') {
+                console.log('获取key:' + key )
+            }
+            return val
+        },
+        set: function (newVal) {
+            //debugger
+            console.log('设置key:' + key + '为:' + newVal)
+            if (newVal === val) return
+            val = newVal
+        }
+    })
+}
+
+let app1 = new Observer({
+    name: 'youngwind',
+    age: 25
+  });
+  
+  let app2 = new Observer({
+    university: 'bupt',
+    major: 'computer'
+  });
+
+app1.data.name
+app1.data.age = 100
+app2.data.university 
+app2.data.major = 'science' 
